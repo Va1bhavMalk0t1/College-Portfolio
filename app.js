e602fe934c0001c9f9e7120f5fad8c5039b30b34
Vaibhav Malkoti <177619278+Va1bhavMalk0t1@users.noreply.github.com>
2025-02-20 11:09:46 +0530

    Changes in Js

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const cr = document.querySelector("#cur") ; 
 const body = document.querySelector("body") ; 
 body.addEventListener("mousemove",(pos)=>{
-    cur.style.left = pos.x + "px" ; 
-    cur.style.top = pos.y + "px" ; 
+    cur.style.left = pos.clientX + "px" ; 
+    cur.style.top = pos.clientY + "px" ; 
 })
 
 var el = document.querySelectorAll(".el") ; 
 
 el.forEach((val) => {
     val.addEventListener("mousemove" , (pos)=>{
-        val.childNodes[3].style.left = pos.x + "px" ; 
+        val.childNodes[3].style.left = pos.clientX + "px" ; 
     })
     val.addEventListener("mouseenter" , ()=>{
         val.childNodes[3].style.opacity= 1 ;  
